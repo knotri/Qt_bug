ac896e73b16ce23c3f1b947c1ae03991ecd527fc
knotri <knotri@gmail.com>
2014-02-19 21:22:39 +0200

    ww

---
+++ widget.cpp
@@ -0,0 +1,118 @@
+#include "widget.h"
+
+std::vector< Bullet> bullet;
+
+void Ship::fire(){
+    Bullet tmp;
+    tmp.x = x;
+    tmp.y = y;
+    tmp.angel = angel+3.1415/2;
+    tmp.speed = 5;
+    bullet.push_back(tmp);
+}
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    QTimer* timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(update()));
+    timer->start(16);
+    ship.x = ship.y = 100;
+    ship.angel = 45;
+    ship.speed = 2;
+    fonImg = QPixmap("fon.jpg");
+
+    QTimer* timer2 = new QTimer(this);
+    connect(timer2,SIGNAL(timeout()),this,SLOT(myUpdate()));
+    timer2->start(16);
+
+    bullet.resize(0);
+
+}
+
+
+void Widget::paintEvent(QPaintEvent *e){
+    ship.move();
+    QPainter p(this);
+    p.drawPixmap(0,0,fonImg);
+    p.setBrush(QColor(200,150,50));
+
+    qWarning() << bullet.size();
+    for(int i = 0; i<bullet.size(); i++){
+        p.drawEllipse(QPointF(bullet[i].x,bullet[i].y),2,2);
+    }
+
+
+    p.translate(QPointF(ship.x,ship.y));
+    p.rotate(ship.angel*180/3.1415);
+    p.drawEllipse(QPointF(0,0),10,5);
+    p.rotate(-ship.angel*180/3.1415);
+    p.translate(-QPointF(ship.x,ship.y));
+
+
+}
+
+void Widget::keyPressEvent(QKeyEvent *e){
+
+    if(e->key() == Qt::Key_W){
+        ship.speed += 0.1;
+        ship.fire();
+    }
+
+    if(e->key() == Qt::Key_D){
+        ship.angel += 0.1;
+        //qWarning() << 5;
+    }
+    if(e->key() == Qt::Key_A){
+        ship.angel -= 0.1;
+    }
+    if(e->key() == Qt::Key_S){
+        ship.speed -= 0.1;
+    }
+
+    if(e->key() == Qt::Key_Space){
+        ship.fire();
+        //qWarning() << 5;
+    }
+}
+
+void Widget::myUpdate(){
+    for(int i=bullet.size()-1; i>=0; i--){
+        bullet[i].move();
+        if(bullet[i].life<0){
+            bullet.erase(bullet.begin()+i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ widget.h
@@ -0,0 +1,73 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QPainter>
+#include <vector>
+#include <qmath.h>
+#include <QTimer>
+#include <QKeyEvent>
+#include <QDebug>
+
+class Bullet{
+public:
+    float x,y,angel,speed;
+    int life;
+    void move(){
+        life--;
+        x+=speed*cos(angel);
+        y+=speed*sin(angel);
+    }
+
+};
+
+class Ship{
+public:
+    float x,y,angel,speed;
+    void move(){
+        x += cos(angel)*speed;
+        y += sin(angel)*speed;
+        speed *= 0.999;
+    }
+    void fire();
+};
+
+class vector {
+    float x,y;
+    float normal(){
+        float dist = sqrt(x*x + y*y);
+        x/=dist;
+        y/=dist;
+
+    }
+
+    float angel(){
+        float aaan;
+        if(y == 0){
+            aaan = 0;
+        }else{
+            aaan = -atan(x/y)+3.1415926/2;
+        }
+        if(y > 0){aaan += 3.1415926;}
+        return aaan;
+    }
+};
+
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    void paintEvent(QPaintEvent *e);
+    void keyPressEvent(QKeyEvent *);
+    Ship ship;
+    QPixmap fonImg;
+
+public slots:
+    void myUpdate();
+
+};
+
+#endif // WIDGET_H
